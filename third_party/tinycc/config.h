a46f9d69fa621864292403cef3048022bd13b444
Peterino2 <peterlimail47@gmail.com>
2026-07-31 19:15:37 -0700

    Vendor TinyCC and plan C backend

---
+++ third_party/tinycc/config.h
@@ -0,0 +1,13 @@
+#ifndef RIBLANG_TINYCC_CONFIG_H
+#define RIBLANG_TINYCC_CONFIG_H
+
+/* Minimal static configuration for the embedded Riblang libtcc build. */
+#define TCC_VERSION "0.9.28-riblang"
+#define CONFIG_TCC_PREDEFS 0
+#define CONFIG_TCC_BACKTRACE 0
+#define CONFIG_TCC_BCHECK 0
+#define CONFIG_TCC_SEMLOCK 0
+#define CONFIG_DWARF_VERSION 0
+#define CONFIG_NEW_MACHO 1
+
+#endif
